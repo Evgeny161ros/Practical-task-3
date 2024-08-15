8052012f172c41d2ce732c4b4da439b942d127cb
Evgeny161ros <rostow.papa.2015@gmail.com>
2024-08-15 23:41:18 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,12 @@
+name = "Evgeny"
+str = 'Name:'
+print(str, name)
+age = 39
+str = 'Age:'
+print(str, age)
+new_age = '4' + '0'
+after_new_year = 'New Age:'
+print(after_new_year, new_age)
+id = 'Is Student:'
+print(id, int(new_age) == age +1)
+
